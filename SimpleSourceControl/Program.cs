a49d168d10ce3602b3129814e4ec20aefbaf6f08
VacaSama <klc_moore@outlook.com>
2025-07-02 16:16:13 -0700

    Add CommonBugs method
    This commit introduces a new `CommonBugs` method . The new method prints a list of common bugs, incorporating literal insects with the possible expectation of software bugs. A developers pun.

--- SimpleSourceControl/Program.cs
+++ SimpleSourceControl/Program.cs
@@ -1,32 +1,46 @@
-﻿/**
+﻿
+
+using System.Net;
+using System.Security.Cryptography;
+
+/**
  * @Author: Kourtnie M.
  * July 2025 - SUMMER 2025
  * This is a super simple source control example, 
  * That I will be using for my first homework assignment in 
  * C#/.NET(CPW 211). 
  * */
-
 public class SimpleSourceControl
 {
     static void Main(String[] args)
     {
         Console.WriteLine("Hello, Source Control!");
-        Changes();
+        String[] badPuns =
+        {
+                "Why do Java developers wear glasses? \nBecause they don’t C#.",
+                "What do computers snack on during break? \nMicrochips and cookies.",
+                "Why was the computer cold? \nIt left its Windows open. "
+        };
+        CommonBugs();
     }
 
-    static void Changes()
+    static void CommonBugs()
     {
         Console.WriteLine("There is a change below");
         Console.WriteLine("Here are some common bugs you may encounter: ");
         String[] bugs = {
-        "Praying Mantis",
-        "Cockroach",
-        "Stink Bug",
-        "Bumble Bee"};
+                "Praying Mantis",
+                "Cockroach",
+                "Stink Bug",
+                "Bumble Bee"};
 
         foreach (String bug in bugs)
         {
             Console.Write(bug + " ");
         }
+        Console.WriteLine();
+        Console.WriteLine("\nI think you were expecting software bugs" +
+            "\nJust a little play on words");
     }
+ 
 }
